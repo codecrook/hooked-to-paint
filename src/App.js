f8cf6bf60df383e1342933fc50eb8194ef7c09ae
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-07-17 20:58:30 +0530

    ADD: Name component to enter/display the name of painting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Name from './components/Name';
 
 function App() {
   return (
     <div>
       <h1>Hooked To Paint</h1>
+      <Name />
     </div>
   );
 }
---
+++ src/components/Name.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './Name.css';
+
+function Name() {
+    const [name, setName] = useState('');
+
+    return (
+        <label className="header-name">
+            <input
+                value={name}
+                onChange={e => setName(e.target.value)}
+                onDoubleClick={e => e.target.setSelectionRange(0, e.target.value.length)}
+                placeholder="Untitled"
+            />
+        </label>
+    );
+}
+
+export default Name;
