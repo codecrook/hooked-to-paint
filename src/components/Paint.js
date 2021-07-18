4319ea6142c1c69f11386fbb72e5cd0dc2258b1a
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-07-19 01:11:08 +0530

    ADD: ColorPicker in paint to pick colors

--- src/components/Paint.js
+++ src/components/Paint.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Paint.css';
 import Name from './Name';
+import ColorPicker from './ColorPicker';
+import randomColor from 'randomcolor';
+
 
 function Paint() {
+    const [colors, setColors] = useState([]);
+    const [activeColor, setActiveColor] = useState(null);
+
+    const getColors = () => {
+        const baseColor = randomColor().slice(1);
+        fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=monochrome`)
+            .then(res => res.json())
+            .then(({ colors }) => {
+                setColors(colors.map(color => color.hex.value));
+                setActiveColor(colors[0].hex.value);
+            });
+    };
+
+    useEffect(getColors, []);
+
     return (
-        <header>
+        <header style={{ borderTop: `10px solid ${activeColor}` }}>
             <div className="app">
                 <Name />
             </div>
             <div className="color-picker-contianer">
-                {/*TODO: Create ColorPicker Component*/}
+                <ColorPicker
+                    colors={colors}
+                    activeColor={activeColor}
+                    setActiveColor={setActiveColor}
+                />
             </div>
         </header>
     );
